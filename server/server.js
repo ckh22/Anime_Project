b280d4733fe1a8368874a947b0efc390c1769cc4
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2021-01-01 15:22:41 -0800

    Added .env file Added dependencies to server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-// Some code
+// Dependencies
+import colors from 'colors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+
+dotenv.congif();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server runnin in ${process.env.NODE_ENV} mode on ${PORT}`.red.bold));
