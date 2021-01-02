a433f94275ffc0f0947ceb5a7f55758f9511a053
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-01-01 16:27:17 -0800

    Adding data & Fixing Login/Register
    Co-Authored-By: Robert Hartley <43013927+RobHart01@users.noreply.github.com>

--- client/src/Pages/Homepage/Homepage.jsx
+++ client/src/Pages/Homepage/Homepage.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import users from '../../Data/users'
 
 export default function Homepage() {
     return (
-        <div>
-            Homepage
-        </div>
+        <div> {
+            users.map((user) => (
+                <div>
+                    <div> {
+                        user.email
+                    } </div>
+                    <div> {
+                        user.name
+                    } </div>
+                    <div> {
+                        user.password
+                    } </div>
+                    <div> {
+                        user.isAdmin
+                    } </div>
+                    <hr/>
+                </div>
+
+            ))
+        } </div>
+
     )
 }
