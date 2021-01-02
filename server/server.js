32855a4a5abd04962200248cdf20a76c8b813537
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2021-01-01 17:40:56 -0800

    Updated a bunch of stuff I think
    Added utils file.
    Added routes to server.
    Added JSON Web Token.
    Added authentication middleware.
    Added error handler middleware.
    Added database logic.
    
    Co-Authored-By: ckh22 <45274762+ckh22@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -2,13 +2,23 @@
 import colors from 'colors';
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
+// import path from 'path';
+import connectDB from './config/db.js';
+
+// Routes
+import userRoutes from './routes/userRoutes.js';
+import animeRoutes from './routes/animeRoutes.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/anime', animeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
