373676ebf6644d8848dd23f7fcfc93eaee64dd99
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2021-01-02 15:35:42 -0800

    REDUX BABYYYYY
    Set up redux for constants, reducers, actions.
    Added basic login, logout, logic, still needs testing.
    
    Co-Authored-By: ckh22 <45274762+ckh22@users.noreply.github.com>

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from 'react-redux'
+import store from './store.js';
 const rootEl = document.getElementById("root");
 
 
 function render() {
   ReactDOM.render(
+    <Provider store = {store}>
       <BrowserRouter>
         <App />
-      </BrowserRouter>,
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 }
