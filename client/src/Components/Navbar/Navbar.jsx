f33f5796e9e5bd571420a9728e1d3f3b9038b2a3
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2021-01-05 20:10:18 -0800

    Added Router Links to Login Page
    Some Navbar work

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,146 +1,136 @@
-import React from 'react'
-import {
-    AppBar,
-    Toolbar,
-    IconButton,
-    Typography,
-    Button
-} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu'
+// Dependencies Imports
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+// Material UI Core Imports
+import { AppBar, Toolbar, IconButton, Typography, Button, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
-import Login from '../Auth/Login'
-import Register from '../Auth/Register'
-import {Link} from 'react-router-dom'
-import './Navbar.css'
+
+// Components Imports
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+
+// Style
+import './Navbar.css';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        color: 'white',
-        background: '#4e4ca8'
-    },
-    iconButton: {
-        marginRight: theme.spacing(2),
-        color: '#4e4ca8'
-    },
-    title: {
-        flexGrow: 1
-    }
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		color: 'white',
+		background: '#4e4ca8',
+	},
+	iconButton: {
+		marginRight: theme.spacing(2),
+		color: '#4e4ca8',
+	},
+	title: {
+		flexGrow: 1,
+	},
 }));
 
 export default function Navbar() {
-    const [login, setLogin] = React.useState(false);
-    const [register, setRegister] = React.useState(false);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    // const [isShown, setIsShown] = React.useState(false);
+	const [login, setLogin] = React.useState(false);
+	const [register, setRegister] = React.useState(false);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	// const [isShown, setIsShown] = React.useState(false);
 
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+	function handleLogin() {
+		setLogin(true);
+	}
 
-    function handleLogin() {
-        setLogin(true);
-    }
+	function handleRegister() {
+		setRegister(true);
+	}
+	const classes = useStyles();
+	return (
+		<div
+			style={{
+				boxShadowBottom: '0px 25px 50px 30px white',
+				zIndex: '10',
+				overflow: 'visible',
+			}}
+		>
+			<AppBar
+				position="static"
+				style={{
+					background: '#000000',
+					color: 'white',
+				}}
+			>
+				<Toolbar>
+					<IconButton edge="start" className={classes.iconButton} color="inherit" aria-label="menu">
+						<MenuIcon />
+					</IconButton>
+					<EmojiEmotionsIcon />
+					<Typography variant="h6" className={classes.title} style={{ paddingLeft: '15px' }}>
+						Company Name
+					</Typography>
+					<ul
+						style={{
+							listStyleType: 'none',
+							marginRight: '30px',
+							padding: '0',
+							overflow: 'hidden',
+						}}
+					>
+						<li className="nav-links">
+							<Link to={`/`} id="links">
+								Home
+							</Link>
+						</li>
+						<li className="nav-links">
+							<Link to={`/animes`} id="links">
+								Animes
+							</Link>
+						</li>
+						<li className="nav-links">
+							<Link to={`/studios`} id="links">
+								Studios
+							</Link>
+						</li>
+						<li className="nav-links">
+							<Link to={`/voiceActors`} id="links">
+								Voice Actors
+							</Link>
+						</li>
 
-    function handleRegister() {
-        setRegister(true);
-    }
-    const classes = useStyles();
-    return (
-        <div style={
-            {
-                boxShadowBottom: '0px 25px 50px 30px white',
-                zIndex: '10',
-                overflow: 'visible'
-            }
-        }>
-            <AppBar position="static"
-                style={
-                    {
-                        background: '#000000',
-                        color: 'white'
-                    }
-            }>
-                <Toolbar>
-                    <IconButton edge="start"
-                        className={
-                            classes.iconButton
-                        }
-                        color="inherit"
-                        aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                    <EmojiEmotionsIcon/>
-                    <Typography variant="h6"
-                        className={
-                            classes.title
-                        }
-                        style={
-                            {paddingLeft: '15px'}
-                    }>
-                        Company Name
-                    </Typography>
-                    <ul style={
-                        {
-                            listStyleType: 'none',
-                            marginRight: '30px',
-                            padding: '0',
-                            overflow: 'hidden'
-                        }
-                    }>
-                        <li className='nav-links'>
-                            <Link to={`/`}
-                                id='links'>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-links'>
-                            <Link to={`/animes`}
-                                id='links'>
-                                Animes
-                            </Link>
-                        </li>
-                        <li className='nav-links'>
-                            <Link to={`/studios`}
-                                id='links'>
-                                Studios
-                            </Link>
-                        </li>
-                        <li className='nav-links'>
-                            <Link to={`/voiceActors`}
-                                id='links'>
-                                Voice Actors
-                            </Link>
-                        </li>
-                        
-                        <li className='nav-links'>
-                            <Link to={`/`}
-                                id='links'>
-                                Want to help?
-                            </Link>
-                        </li>
-                    </ul>
-                    <Button color="inherit"
-                        onClick={handleLogin}
-                        className={
-                            classes.menuButton
-                    }>Login</Button>
-                    <Button color="inherit"
-                        onClick={handleRegister}
-                        className={
-                            classes.menuButton
-                    }>Register</Button>
-                </Toolbar>
-            </AppBar>
-            <Login open={login}
-                setOpen={setLogin}/>
-            <Register open={register}
-                setOpen={setRegister}/>
-        </div>
-    )
+						<li className="nav-links">
+							<Link to={`/`} id="links">
+								Want to help?
+							</Link>
+						</li>
+					</ul>
+					<Button
+						component={RouterLink}
+						to="/login"
+						color="inherit"
+						onClick={handleLogin}
+						className={classes.menuButton}
+					>
+						Login
+					</Button>
+					<Button
+						component={RouterLink}
+						to="/register"
+						color="inherit"
+						onClick={handleRegister}
+						className={classes.menuButton}
+					>
+						Register
+					</Button>
+				</Toolbar>
+			</AppBar>
+			<Login open={login} setOpen={setLogin} />
+			<Register open={register} setOpen={setRegister} />
+		</div>
+	);
 }
