68b1517f55fb4ec90d5a3b72df3d8e0875be8c40
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-02-09 05:54:08 -0800

    formik

--- client/src/Components/Form/AnimeForm.jsx
+++ client/src/Components/Form/AnimeForm.jsx
@@ -1,109 +1,71 @@
 import React from 'react'
-import {useForm, useField, splitFormProps} from "react-form";
-
-
-async function sendToFakeServer(values) {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return values;
-}
-
-function validateAddressStreet(value) {
-    if (! value) {
-        return "A street is required";
-    }
-    return false;
-}
-
-async function fakeCheckValidName(name, instance) {
-    if (! name) {
-        return "A name is required";
-    }
-
-    return instance.debounce(async () => {
-        console.log("checking name");
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        // All names are valid, so return a false error
-        return false;
-    }, 500);
-}
-
-const InputField = React.forwardRef((props, ref) => { // Let's use splitFormProps to get form-specific props
-    const [field, fieldOptions, rest] = splitFormProps(props);
-
-    // Use the useField hook with a field and field options
-    // to access field state
-    const {
-        meta: {
-            error,
-            isTouched,
-            isValidating
-        },
-        getInputProps
-    } = useField(field, fieldOptions);
-
-    // Build the field
-    return (
-        <>
-            <input {...getInputProps({ ref, ...rest })}/>{" "}
-            {
-            isValidating ? (
-                <em>Validating...</em>
-            ) : isTouched && error ? (
-                <em>{error}</em>
-            ) : null
-        } </>
-    );
-});
+import {Formik} from 'formik';
 
 const AnimeForm = () => {
-    const {
-        Form,
-        meta: {
-            isSubmitting,
-            canSubmit
-        }
-    } = useForm({
-        onSubmit: async (values, instance) => {
-            // onSubmit (and everything else in React Form)
-            // has async support out-of-the-box
-            await sendToFakeServer(values);
-            console.log("Huzzah!");
-        },
-        debugForm: true
-    });
-
     return (
-        <Form style={{position: 'relative', zIndex: '10', padding: '15px'}}>
-            <div>
-                <label>
-                    Name:
-                    <InputField field="name"
-                        validate={fakeCheckValidName}/>
-                </label>
-            </div>
-            <div>
-                <label>
-                    Address Street:{" "}
-                    <InputField field="address.street"
-                        validate={validateAddressStreet}/>
-                </label>
-            </div>
-
-            <div>
-                <button type="submit"
-                    disabled={
-                        !canSubmit
-                }>
-                    Submit
-                </button>
-            </div>
-
-            <div>
-                <em>{
-                    isSubmitting ? "Submitting..." : null
-                }</em>
-            </div>
-        </Form>
+        <div>
+            <h1>Anywhere in your app!</h1>
+            <Formik initialValues={
+                    {
+                        email: '',
+                        password: ''
+                    }
+                }
+                validate={
+                    values => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+                        return errors;
+                    }
+                }
+                onSubmit={
+                    (values, {setSubmitting}) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }
+            }>
+                {
+                ({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    /* and other goodies */
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={
+                                values.email
+                            }/> {
+                        errors.email && touched.email && errors.email
+                    }
+                        <input type="password" name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={
+                                values.password
+                            }/> {
+                        errors.password && touched.password && errors.password
+                    }
+                        <button type="submit"
+                            disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )
+            } </Formik>
+        </div>
     )
 }
 
